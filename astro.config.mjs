1f80947c1d372fdbbe96545a14b619964b515878
ivanadob <ivana.dob@gmail.com>
2024-09-05 11:42:38 +0200

    fix image prefix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   server: {
-    port: 3000
+    port: 3000,
   },
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: "https://acdh-oeaw.github.io",
+  base: "rest-in-peace-service",
 });
